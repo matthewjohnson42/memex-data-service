a1523e0dfa36f99e5f5ac0fb368d85aa9fc82d96
Matthew Johnson <mjohnson@omega-fortytwo.local>
2021-08-22 15:12:06 -0500

    Google Driver Transferer - repo snapshot - add file writer

--- app/src/main/java/com/matthewjohnson42/memex/dataservice/logic/thread/MongoToGoogleDriveTransferRunner.java
+++ app/src/main/java/com/matthewjohnson42/memex/dataservice/logic/thread/MongoToGoogleDriveTransferRunner.java
@@ -8,7 +8,17 @@ import com.matthewjohnson42.memex.dataservice.data.enumeration.ThreadStatus;
 import com.matthewjohnson42.memex.dataservice.data.mongo.service.ThreadMongoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Instant;
 
 public class MongoToGoogleDriveTransferRunner<ID, D extends DtoForEntity<ID>, E extends Entity<ID>>
         extends MemexDataServiceRunnable {
@@ -18,6 +28,7 @@ public class MongoToGoogleDriveTransferRunner<ID, D extends DtoForEntity<ID>, E 
     private final MongoRepository<E, ID> mongoRepository;
     private final DtoEntityConverter<ID, D, E> mongoDtoConverter;
     private final Drive drive;
+    private final File tempFile;
 
     private final int totalCount;
 
@@ -31,6 +42,7 @@ public class MongoToGoogleDriveTransferRunner<ID, D extends DtoForEntity<ID>, E 
         this.mongoDtoConverter = mongoDtoConverter;
         this.drive = drive;
         this.totalCount = (int) mongoRepository.count();
+        this.tempFile = new File(String.format("mongo-to-drive-transfer-temp-file-%s-%s.tmp", Instant.now().getEpochSecond(), this.hashCode()));
     }
 
     @Override
@@ -38,6 +50,24 @@ public class MongoToGoogleDriveTransferRunner<ID, D extends DtoForEntity<ID>, E 
         super.setupRun();
         // iteratively copy all rawText using pageable, write to temp file
             // update ThreadMongo to include attribute "description", include temp file name in description
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(tempFile);
+        } catch (IOException e) {
+            throw new RuntimeException(String.format("Could not open file %s", tempFile.getAbsolutePath()));
+        }
+        PrintWriter writer = new PrintWriter(fos, true);
+        final int pageSize = 100;
+        final long pagesToWrite = mongoRepository.count() / pageSize;
+        for (int i = 0; i <= pagesToWrite; i++) {
+            Pageable pageable = PageRequest.of(i, pageSize, Sort.Direction.ASC, "createDateTime");
+            Page<E> page = mongoRepository.findAll(pageable);
+            for(E entity: page.getContent()) {
+                writer.println(entity.toString()); // todo need to add JSON generation
+            }
+            logger.info("Wrote page {} of {}", i + 1, pagesToWrite + 1);
+        }
+        writer.close();
         // get total bytes in file
         // set batch number to batches of 1 MB
         // create file on Google Drive
@@ -46,7 +76,7 @@ public class MongoToGoogleDriveTransferRunner<ID, D extends DtoForEntity<ID>, E 
     public boolean executeBatch() {
         // confirm previous batch was successful; if not, re-run batch
         // retrieve bytes from file, send HTTP request with bytes as content
-        return false;
+        return true;
     }
 
     public void updateRun() {
